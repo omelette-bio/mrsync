c26fd226cf83d865d9da097d0f0a6b0e3ce402a1
omelette <omelettebio@proton.me>
2023-04-13 11:02:16 +0200

    modified:   filelist.py

--- filelist.py
+++ filelist.py
@@ -5,39 +5,26 @@ parser.add_argument("path", help="path to list files from")
 args = parser.parse_args()
 
 
-def list_files(path, mode="normal"):
+def list_files(path, recursive=False):
    os.chdir(path)
    print(os.getcwd())
    directories = [path]
    files = []
 
-   # List all files and if there is a directory list the files inside it
    while len(directories) > 0:
       for i in os.listdir(os.getcwd()):
          if i[0] == "." or i[0] == "_" or i[0] == "~":
             pass
-         elif os.path.isdir(i) and mode == "recursive":
+         elif os.path.isdir(i) and recursive:
             directories.append(os.path.join(os.getcwd()) + "/" + i)
          else:
             files.append(i)
-      if mode == "recursive":
+      if recursive:
          os.chdir(directories.pop())
       else:
          directories.pop()
    return(files)
 
-"""
-def list_files(path):
-   os.chdir(path)
-   print(os.getcwd())
-   files = []
 
-   for i in os.listdir(os.getcwd()):
-      if os.path.isfile(i):
-         files.append(i)
-
-   return(files)
-"""
-
-print(list_files(args.path, "recursive"))
+print(list_files(args.path, True))
 print(list_files(args.path))
