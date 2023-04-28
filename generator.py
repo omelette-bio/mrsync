6b534a83ff4058f889ae43feb61526170b28b400
omelette <omelettebio@proton.me>
2023-04-28 21:17:57 +0200

    modified:   generator.py modified:   mrsync.py

--- generator.py
+++ generator.py
@@ -8,18 +8,32 @@ def sort_by_path(dictionnary):
 
 
 # function to compare two lists of files and return the list of files to send, modify and delete
-def compare(a, b):
+def compare(a, b, nb_source):
    list_to_send = []
    list_to_modify = []
    list_to_delete = []
-   for i in a:
-      #i = os.path.join(os.path.basename(a[i][0]),i)
-      if i in b:
-         if a[i][2] > b[i][2]:
-            list_to_modify.append(i)
-      else:
-         list_to_send.append(i)
-   for i in b:
-      if i not in a:
-         list_to_delete.append(i)
+   if nb_source == 1:
+      for i in a:
+         if i in b:
+            if a[i][2] > b[i][2]:
+               list_to_modify.append(i)
+         else:
+            list_to_send.append(i)
+      for i in b:
+         if i not in a:
+            list_to_delete.append(i)
+   else:
+      # if the source has multiple folders, we need to compare the files in the same folder
+      # and create the relative path of the file
+      for files in a:
+         file = os.path.join(os.path.basename(a[files][0]),files)
+         if file in b:
+            if a[files][2] > b[file][2]:
+               list_to_modify.append(file)
+         else:
+            list_to_send.append(file)
+      
+         
+      
+      
    return list_to_send, list_to_modify, list_to_delete
--- mrsync.py
+++ mrsync.py
@@ -29,8 +29,10 @@ if os.fork() == 0:
    # generator to send files
    if os.fork() == 0:
       # create the list of files to send, modify and delete
-      send, modify, delete = generator.compare(files_to_send, destination_files)
+      send, modify, delete = generator.compare(files_to_send, destination_files, len(args.source))
 
+      
+      
       state = "send"
       # we send some requests to the client, if it's not empty
       if len(send) > 0:
@@ -62,10 +64,11 @@ if os.fork() == 0:
       
          if tag == "sendfile":
             #check if the folder exists, if not create it
-            if not os.path.isdir(folder):
-               os.makedirs(folder)
-               if args.verbose > 0:
-                  print(f"Creating folder {folder}", file=sys.stderr)
+            if folder != "":
+               if not os.path.isdir(folder):
+                  os.makedirs(folder)
+                  if args.verbose > 0:
+                     print(f"Creating folder {folder}", file=sys.stderr)
             
             #create the file
             file = os.path.join(folder, os.path.basename(file))
@@ -83,6 +86,30 @@ if os.fork() == 0:
             os.write(1, data)
             os.close(currentfile)
 
+         if tag == "modifyfile":
+            #check if the folder exists, if not create it
+            if folder != "":
+               if not os.path.isdir(folder):
+                  os.makedirs(folder)
+                  if args.verbose > 0:
+                     print(f"Creating folder {folder}", file=sys.stderr)
+            
+            #create the file
+            file = os.path.join(folder, os.path.basename(file))
+            currentfile = os.open(file, os.O_CREAT | os.O_WRONLY)
+            
+            if args.verbose > 0:
+               print(f"Receiving {file}...", file=sys.stderr)
+            
+            #change the standard output to the file
+            
+            os.dup2(currentfile, 1)
+            
+            #write the data
+            
+            os.write(1, data)
+            os.close(currentfile)
+         
       elif tag == "endfile" and args.verbose > 0:
          print(f"Done", file=sys.stderr)
          print("")
@@ -144,13 +171,19 @@ if os.fork() == 0:
          while True:
             data = os.read(sending_file, 16*1024*1024)
             
+            folder = ""
             
-            if os.path.dirname(file) == "":
-               folder = os.path.basename(files[file][0])
+            if len(args.source) > 1:
+               if os.path.dirname(file) == "":
+                  folder = os.path.basename(files[file][0])
+               
+               else:
+                  folder = os.path.join(os.path.basename(files[file][0]), os.path.dirname(file))
             
             else:
-               folder = os.path.join(os.path.basename(files[file][0]), os.path.dirname(file))
-            
+               if os.path.dirname(file) != "":
+                  folder = os.path.dirname(file)
+                  
             message.send(fdw1, "sendfile", (file, folder, data))
             
             # send "endfile" if there the file has been read entirely
@@ -162,7 +195,48 @@ if os.fork() == 0:
       
       message.send(fdw1, "end", "end")
       
-   
+   if modify_list != []:
+      
+      for file in modify_list:
+         
+         full_path = os.path.join(files[file][0], file)
+         
+         if args.verbose > 0:
+            print(f"Sending : {full_path}")
+            
+         sending_file = os.open(full_path, os.O_RDONLY)
+         
+         if args.verbose > 0:
+            print(f"Reading...")
+            print("")
+            
+         # read the file and send it, in multiple parts if size > 16 Mo
+         while True:
+            data = os.read(sending_file, 16*1024*1024)
+            
+            folder = ""
+            
+            if len(args.source) > 1:
+               if os.path.dirname(file) == "":
+                  folder = os.path.basename(files[file][0])
+               
+               else:
+                  folder = os.path.join(os.path.basename(files[file][0]), os.path.dirname(file))
+            
+            else:
+               if os.path.dirname(file) != "":
+                  folder = os.path.dirname(file)
+                  
+            message.send(fdw1, "modifyfile", (file, folder, data))
+            
+            # send "endfile" if there the file has been read entirely
+            if len(data) < 16*1024*1024:
+               message.send(fdw1, "endfile", "endfile")
+               break
+         
+         os.close(sending_file)
+      
+      message.send(fdw1, "end", "end")
    
    
    os.close(fdw1)
